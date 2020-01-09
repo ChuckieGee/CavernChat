fcb21a788741079505f9a12b05b398d8bbac7bf4
ChuckieGee <33033219+ChuckieGee@users.noreply.github.com>
2020-01-09 21:03:26 +0000

    Adding some tests

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ describe('Server Status Test', function(){
 });
 
 describe('Webpages Tests', function(){
-    it('This should returns true if "Rooms" loads correctly', function(done){
+    it('This should return pass if "Rooms" loads correctly', function(done){
         request('http://localhost:3000/rooms', function(err, res, body){
             var bigString = res.body;
             var searchString = bigString.search("<title>Socket.io Rooms</title>")
@@ -22,10 +22,34 @@ describe('Webpages Tests', function(){
             done();
         })
     });
-    it('This should returns pass if "index" loads correctly.', function(done){
+    it('This should return pass if "index" loads correctly.', function(done){
         request('http://localhost:3000/', function(err, res, body){
             var bigString = res.body;
             var searchString = bigString.search("<title>Mongo Chat with Socket.io</title>")
+            expect(searchString).to.not.equal(-1);
+            done();
+        })
+    });
+    it('This should return pass if "friends" loads correctly.', function(done){
+        request('http://localhost:3000/friends', function(err, res, body){
+            var bigString = res.body;
+            var searchString = bigString.search("<title>Friends Demo</title>")
+            expect(searchString).to.not.equal(-1);
+            done();
+        })
+    });
+    it('This should return pass if "friends" loads correctly.', function(done){
+        request('http://localhost:3000/about', function(err, res, body){
+            var bigString = res.body;
+            var searchString = bigString.search("<title>About</title>")
+            expect(searchString).to.not.equal(-1);
+            done();
+        })
+    });
+    it('This should return pass if the 404 page loads correctly.', function(done){
+        request('http://localhost:3000/*', function(err, res, body){
+            var bigString = res.body;
+            var searchString = bigString.search("<title>404</title>")
             expect(searchString).to.not.equal(-1);
             done();
         })
